0b35ce075f0713178dcdd80dd23f32b6525f3641
hakim-mis <hakim.rajshahi@gmail.com>
2026-06-13 17:25:23 +0600

    update

--- Controllers/LeaderboardController.cs
+++ Controllers/LeaderboardController.cs
@@ -4,7 +4,6 @@ using FootballPredictionGame.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace FootballPredictionGame.Controllers;
 
@@ -18,12 +17,33 @@ public class LeaderboardController : Controller
         _userManager = userManager;
     }
 
-    public async Task<IActionResult> Index()
+    public async Task<IActionResult> Index(string range = "1-50")
     {
         var players = await _userManager.GetUsersInRoleAsync("User");
 
-        var model = LeaderboardRankingHelper.Build(players, take: 100);
+        var allRankedUsers = LeaderboardRankingHelper.Build(players, take: players.Count);
 
-        return View(model);
+        var startSl = 1;
+
+        var filteredModel = range switch
+        {
+            "51-100" => allRankedUsers.Skip(50).Take(50).ToList(),
+            "101-150" => allRankedUsers.Skip(100).Take(50).ToList(),
+            "rest" => allRankedUsers.Skip(150).ToList(),
+            _ => allRankedUsers.Take(50).ToList()
+        };
+
+        startSl = range switch
+        {
+            "51-100" => 51,
+            "101-150" => 101,
+            "rest" => 151,
+            _ => 1
+        };
+
+        ViewBag.Range = range;
+        ViewBag.StartSl = startSl;
+
+        return View(filteredModel);
     }
 }
